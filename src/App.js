ba71e91cc0b428d557a0804ff82f3268213ee517
Jimmique <jimmique88@gmail.com>
2019-08-13 20:12:04 -0700

    Created form.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form.js';
 
 function App() {
+
+  const [ teamMembers, setTeamMembers ] = useState(
+    [{
+      name: 'Jimmique Parsons',
+      email: 'jimmique88@gmail.com', 
+      role: 'Web Developer'
+    }]
+  );
+
+  const addNewMember = (member) => {
+    setTeamMembers([...teamMembers, member]);
+  };
+
   return (
     <div className="App">
-
+    <h1>The Team</h1>
+      {teamMembers.map( member => {
+        return (
+          <div>
+            <p>Name: {member.name}</p>
+            <p>Email: {member.email}</p>
+            <p>Role: {member.role}</p>
+          </div>
+        )
+      })}
+      <Form addNewMemeber={addNewMember} />
     </div>
   );
 }
